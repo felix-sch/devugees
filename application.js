afad628ee3ee886761b84d3423996cc5131fff07
Thomas Kuhnert <tfkuhnert@gmail.com>
2017-07-03 14:21:08 +0200

    collapsing list and change color

--- application.js
+++ application.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        $("p").hide();
+    $(".btn1").click(function(){
+      $(".recipie1").toggle(500);
+    });
+
+    $(".btn2").click(function(){
+        $(".recipie2").toggle(500);
+    });
+    $(".remove").click(function(){
+        $("li").toggleClass('red');
     });
 });
